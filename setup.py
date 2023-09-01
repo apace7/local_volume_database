c1db24fea20f29892525d33d832fbd2cf5fd9148
Sergey E. Koposov <skoposov@ed.ac.uk>
2023-09-01 19:38:20 +0100

    the package doesn't pip install without version and with non-existant package

--- setup.py
+++ setup.py
@@ -10,21 +10,24 @@ import os
 from setuptools import setup
 
 _name = "local_volume_database"
-_version = ""
+_version = "0.0.1"
 
 setup(
     name=_name,
     version=_version,
     author='Andrew B. Pace',
     author_email='pvpace1@gmail.com',
-    packages=['local_volume_database'],
-    package_data={'data/':['*.csv'],},
+    packages=[],  # 'local_volume_database',
+    package_data={
+        'data/': ['*.csv'],
+    },
     url='https://github.com/apace7/local_volume_database',
     license='LICENSE',
-    description='Database of the properties of local field dwarf galaxies and Milky Way Star Cluster',
+    description=
+    'Database of the properties of local field dwarf galaxies and Milky Way Star Cluster',
     long_description=open('README.md').read(),
     install_requires=[
-      "numpy",
-      "astropy",
+        "numpy",
+        "astropy",
     ],
 )
