132a893a2f45e00f7bf10c7e02a79e9e2de885f2
Andrew Pace <pvpace1@gmail.com>
2023-09-08 01:42:49 -0400

    split all tables into yaml files; fix some column names; combine table working with masks; add value added columns

--- code/combine_table.py
+++ code/combine_table.py
@@ -16,9 +16,14 @@ items_to_join = dsph_mw['key']
 ## this is not the right way to do this
 ## this needs to be changed such that the table is made with masked columns
 # np.ma.masked_all(len(items_to_join), dtype=type_for_column)
-comb_table = table.Table(np.zeros((len(items_to_join), 76)),names=('key', 'name','host','confirmed_real', 'confirmed_dwarf', 'ra', 'dec', 'rhalf', 'rhalf_em', 'rhalf_ep', 'position_angle', 'position_angle_em', 'position_angle_ep', 'ellipticity', 'ellipticity_em', 'ellipticity_ep', 'ellipticity_ul', 'ref_structure', 'distance_modulus', 'distance_modulus_em', 'distance_modulus_ep', 'ref_distance', 'apparent_magnitude_v', 'apparent_magnitude_v_em', 'apparent_magnitude_v_ep', 'ref_m_v', 'vlos_systemic', 'vlos_systemic_em', 'vlos_systemic_ep', 'vlos_sigma', 'vlos_sigma_em', 'vlos_sigma_ep', 'vlos_sigma_ul', 'ref_velocity', 'pmra', 'pmra_em', 'pmra_ep', 'pmdec', 'pmdec_em', 'pmdec_ep', 'ref_proper_motion', 'spec_metallicity', 'spec_metallicity_em', 'spec_metallicity_ep', 'spec_metallicity_sigma', 'spec_metallicity_sigma_em', 'spec_metallicity_sigma_ep', 'spec_metallicity_sigma_ul', 'ref_spec_metallicity', 'rcore', 'rcore_em', 'rcore_ep', 'rking', 'rking_em', 'rking_ep', 'ref_king', 'rad_sersic', 'rad_sersic_em', 'rad_sersic_ep', 'n_sersic', 'n_sersic_em', 'n_sersic_ep', 'ref_sersic', 'age', 'age_em', 'age_ep', 'ref_age', 'metallicity_photometric', 'metallicity_photometric_em', 'metallicity_photometric_ep', 'ref_metallicity_photometric', 'flux_HI', 'flux_HI_em', 'flux_HI_ep', 'flux_HI_ul', 'ref_flux_HI' ), dtype=('U100', 'U100','U100','i1', 'i1','f8', 'f8','f8', 'f8', 'f8','f8', 'f8', 'f8','f8', 'f8', 'f8', 'f8', 'U100', 'f8', 'f8', 'f8', 'U100', 'f8', 'f8', 'f8', 'U100', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'U100', 'f8', 'f8' ,'f8', 'f8', 'f8', 'f8', 'U100', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'U100', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'U100', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'U100', 'f8', 'f8', 'f8', 'U100', 'f8', 'f8', 'f8', 'U100', 'f8', 'f8', 'f8', 'f8','U100'))
+comb_table = table.Table(np.zeros((len(items_to_join), 3)),names=('key', 'ra', 'dec' ), dtype=('U100','f8', 'f8', ))
+col_name = ['name','host','confirmed_real', 'confirmed_dwarf',  'rhalf', 'rhalf_em', 'rhalf_ep', 'position_angle', 'position_angle_em', 'position_angle_ep', 'ellipticity', 'ellipticity_em', 'ellipticity_ep', 'ellipticity_ul', 'ref_structure', 'distance_modulus', 'distance_modulus_em', 'distance_modulus_ep', 'ref_distance', 'apparent_magnitude_v', 'apparent_magnitude_v_em', 'apparent_magnitude_v_ep', 'ref_m_v', 'vlos_systemic', 'vlos_systemic_em', 'vlos_systemic_ep', 'vlos_sigma', 'vlos_sigma_em', 'vlos_sigma_ep', 'vlos_sigma_ul', 'ref_vlos', 'pmra', 'pmra_em', 'pmra_ep', 'pmdec', 'pmdec_em', 'pmdec_ep', 'ref_proper_motion', 'spec_metallicity', 'spec_metallicity_em', 'spec_metallicity_ep', 'spec_metallicity_sigma', 'spec_metallicity_sigma_em', 'spec_metallicity_sigma_ep', 'spec_metallicity_sigma_ul', 'ref_spec_metallicity', 'rcore', 'rcore_em', 'rcore_ep', 'rking', 'rking_em', 'rking_ep', 'ref_king', 'rad_sersic', 'rad_sersic_em', 'rad_sersic_ep', 'n_sersic', 'n_sersic_em', 'n_sersic_ep', 'ref_sersic', 'age', 'age_em', 'age_ep', 'ref_age', 'metallicity_photometric', 'metallicity_photometric_em', 'metallicity_photometric_ep', 'ref_metallicity_photometric', 'flux_HI', 'flux_HI_em', 'flux_HI_ep', 'flux_HI_ul', 'ref_flux_HI']
+col_type = [ 'U100','U100','i1', 'i1','f8', 'f8', 'f8','f8', 'f8', 'f8','f8', 'f8', 'f8', 'f8', 'U100', 'f8', 'f8', 'f8', 'U100', 'f8', 'f8', 'f8', 'U100', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'U100', 'f8', 'f8' ,'f8', 'f8', 'f8', 'f8', 'U100', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'U100', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'U100', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'U100', 'f8', 'f8', 'f8', 'U100', 'f8', 'f8', 'f8', 'U100', 'f8', 'f8', 'f8', 'f8','U100']
 
+for i,j in zip(col_name, col_type):
+    comb_table[i] = np.ma.masked_all(len(items_to_join), dtype=j)
 
+missing_key = []
 for i in range(len(items_to_join)):
     with open("data_input/" + items_to_join[i] + '.yaml', 'r') as stream:
         try:
@@ -32,15 +37,18 @@ for i in range(len(items_to_join)):
                     x = list(stream_yaml[y].keys())
                     for list_key in range(len(x)):
                         name = x[list_key]
-                        if x[list_key] == 'ref':
-                            name = 'ref_' + y
+
                         if name not in comb_table.dtype.names:
-#                             print(x[list_key])
+                            missing_key.append(name)
+
                             continue
                         else:
                             comb_table[name][i] = stream_yaml[y][x[list_key]]
+
         except yaml.YAMLError as exc:
             print(exc)
+
+print(Counter(missing_key))
 
 def dist_mod(mu, mu_em=0, mu_ep=0):
     def dm(x):
@@ -62,5 +70,21 @@ comb_table['rhalf_physical_sph'] = comb_table['rhalf_physical']*np.sqrt(1.-comb_
 
 comb_table['mass_HI'] = np.log10(235600 * comb_table['flux_HI']*(comb_table['distance']/1000.)**2 )
 
+comb_table['metallicity'] = np.ma.masked_all(len(comb_table), dtype=float)
+comb_table['metallicity_em'] = np.ma.masked_all(len(comb_table), dtype=float)
+comb_table['metallicity_ep'] = np.ma.masked_all(len(comb_table), dtype=float)
+comb_table['metallicity_type'] = np.ma.masked_all(len(comb_table), dtype='U100')
+for i in range(len(comb_table)):
+    if ma.is_masked(comb_table['spec_metallicity'][i])==False:
+        comb_table['metallicity'][i] = comb_table['spec_metallicity'][i]
+        comb_table['metallicity_em'][i] = comb_table['spec_metallicity_em'][i]
+        comb_table['metallicity_ep'][i] = comb_table['spec_metallicity_ep'][i]
+        comb_table['metallicity_type'][i] = 'spectroscopic'
+    elif  ma.is_masked(comb_table['metallicity_photometric'][i])==False:
+        comb_table['metallicity'][i] = comb_table['metallicity_photometric'][i]
+        comb_table['metallicity_em'][i] = comb_table['metallicity_photometric_em'][i]
+        comb_table['metallicity_ep'][i] = comb_table['metallicity_photometric_ep'][i]
+        comb_table['metallicity_type'][i] = 'photometric'
+    
 
-comb_table.write('data_output/dsph_mw.csv')
+comb_table.write('data_output/dwarf_mw.csv', overwrite=True)
--- code/create_yaml.py
+++ code/create_yaml.py
@@ -10,113 +10,130 @@ from collections import Counter
 import numpy.ma as ma
 import yaml
 
-dsph_mw = table.Table.read('data/dwarf_mw.csv')
+tables_to_split = ['dwarf_mw.csv', 'dwarf_local_field.csv', 'dwarf_m31.csv', 'gc_disk.csv', 'gc_harris.csv', 'gc_ufsc.csv']
+
 name_discovery = table.Table.read('data/name_discovery.csv')
 notes = table.Table.read('data/notes.csv')
 
-dsph_mw_name = table.join(dsph_mw, name_discovery, keys='key')
-print(len(dsph_mw_name))
+for table_to_load in tables_to_split:
+    loaded_table = table.Table.read('data/' + table_to_load)
+    loaded_table_name = table.join(loaded_table, name_discovery, keys='key')
+    print(table_to_load, len(loaded_table_name), len(loaded_table))
 
-for i in range(len(dsph_mw_name)):
-    d_t = {'key': str(dsph_mw_name['key'][i]) }
-    
-    location = {'ra':float(dsph_mw_name['ra'][i]), 'dec':float(dsph_mw_name['dec'][i])}
-    d_t['location'] = location
-    
-    discovery = {'name':str(dsph_mw_name['name'][i]), 'discovery_year': int(dsph_mw_name['discovery_year'][i])}
-    for prop in ['ref_discovery_1', 'ref_discovery_2', 'ref_discovery_3', 'other_name_1', 'other_name_2', 'other_name_3','other_name_4', 'other_name_5', 'other_name_6', 'table', 'host']:
-        if prop in dsph_mw_name.dtype.names and ma.is_masked(dsph_mw_name[prop][i])==False:
-            discovery[prop] = str(dsph_mw_name[prop][i])
-    for prop in ['confirmed_real', 'confirmed_dwarf', 'confirmed_star_cluster']:
-        if prop in dsph_mw_name.dtype.names and ma.is_masked(dsph_mw_name[prop][i])==False:
-            discovery[prop] = int(dsph_mw_name[prop][i])
-            
-    d_t['name_discovery'] = discovery
-    
-    if ma.is_masked(dsph_mw_name['ref_structure'][i])==False:
-        structure = {'ref': str(dsph_mw_name['ref_structure'][i]) }
-        for prop in ['rhalf', 'rhalf_em', 'rhalf_ep', 'position_angle', 'position_angle_em', 'position_angle_ep', 'ellipticity', 'ellipticity_em', 'ellipticity_ep', 'ellipticity_ul',]:
-            if prop in dsph_mw_name.dtype.names and ma.is_masked(dsph_mw_name[prop][i])==False:
-                structure[prop] = float(dsph_mw_name[prop][i])
-        d_t['structure'] = structure
-    
-    if ma.is_masked(dsph_mw_name['ref_distance'][i])==False:
-        distance = {'ref': str(dsph_mw_name['ref_distance'][i]) }
-        for prop in ['distance', 'distance_em', 'distance_ep', 'distance_modulus', 'distance_modulus_em', 'distance_modulus_ep',]:
-            if prop in dsph_mw_name.dtype.names and ma.is_masked(dsph_mw_name[prop][i])==False:
-                distance[prop] = float(dsph_mw_name[prop][i])
-        d_t['distance'] = distance
-    
-    if ma.is_masked(dsph_mw_name['ref_mv'][i])==False:
-        MV = {'ref':str(dsph_mw_name['ref_mv'][i]), }
-        for prop_out,prop in zip( ['apparent_magnitude_v', 'apparent_magnitude_v_em', 'apparent_magnitude_v_ep'],['apparent_magnitude_V', 'apparent_magnitude_V_em', 'apparent_magnitude_V_ep']):
-            if prop in dsph_mw_name.dtype.names and ma.is_masked(dsph_mw_name[prop][i])==False:
-                MV[prop_out] = float(dsph_mw_name[prop][i])
-        d_t['m_v'] = MV
-    
-    if ma.is_masked(dsph_mw_name['ref_vlos'][i])==False:
-        vlos = {'ref': str(dsph_mw_name['ref_vlos'][i]) }
-        for prop in ['vlos_systemic', 'vlos_systemic_em', 'vlos_systemic_ep', 'vlos_sigma', 'vlos_sigma_em', 'vlos_sigma_ep', 'vlos_sigma_ul',]:
-            if prop in dsph_mw_name.dtype.names and ma.is_masked(dsph_mw_name[prop][i])==False:
-                vlos[prop] = float(dsph_mw_name[prop][i])
-        d_t['velocity'] = vlos
-    
-    if ma.is_masked(dsph_mw_name['ref_proper_motion'][i])==False:
-        pm = {'ref': str(dsph_mw_name['ref_proper_motion'][i]) }
-        for prop in ['pmra', 'pmra_em', 'pmra_ep', 'pmdec', 'pmdec_em', 'pmdec_ep', ]:
-            if prop in dsph_mw_name.dtype.names and ma.is_masked(dsph_mw_name[prop][i])==False:
-                pm[prop] = float(dsph_mw_name[prop][i])
-        d_t['proper_motion'] = pm
-    
-    if ma.is_masked(dsph_mw_name['ref_metallicity'][i])==False:
-        spec_metallicity = {'ref': str(dsph_mw_name['ref_metallicity'][i]) }
-        for prop in ['metallicity', 'metallicity_em', 'metallicity_ep', 'metallicity_sigma', 'metallicity_sigma_em', 'metallicity_sigma_ep', ]:
-            if prop in dsph_mw_name.dtype.names and ma.is_masked(dsph_mw_name[prop][i])==False:
-                spec_metallicity['spec_'+prop] = float(dsph_mw_name[prop][i])
-        d_t['spec_metallicity'] = spec_metallicity
-    
-    if ma.is_masked(dsph_mw_name['ref_king'][i])==False:
-        king = {'ref': str(dsph_mw_name['ref_king'][i])}
-        for prop in ['rcore', 'rcore_em', 'rcore_ep', 'rking', 'rking_em', 'rking_ep', 'king_concentration', 'king_concentration_em', 'king_concentration_ep', ]:
-            if prop in dsph_mw_name.dtype.names and ma.is_masked(dsph_mw_name[prop][i])==False:
-                king[prop] = float(dsph_mw_name[prop][i])
-        d_t['structure_king'] = king
-    
-    if ma.is_masked(dsph_mw_name['ref_sersic'][i])==False:
-        sersic = {'ref': str(dsph_mw_name['ref_sersic'][i])}
-        for prop in ['rad_sersic', 'rad_sersic_em', 'rad_sersic_ep', 'n_sersic', 'n_sersic_em', 'n_sersic_ep',  ]:
-            if prop in dsph_mw_name.dtype.names and ma.is_masked(dsph_mw_name[prop][i])==False:
-                sersic[prop] = float(dsph_mw_name[prop][i])
-        d_t['structure_sersic'] = sersic
-    
-    if ma.is_masked(dsph_mw_name['ref_age'][i])==False:
-        age = {'ref': str(dsph_mw_name['ref_age'][i])}
-        for prop in ['age', 'age_em', 'age_ep',   ]:
-            if prop in dsph_mw_name.dtype.names and ma.is_masked(dsph_mw_name[prop][i])==False:
-                age[prop] = float(dsph_mw_name[prop][i])
-        d_t['age'] = age
-    
-    if ma.is_masked(dsph_mw_name['ref_metallicity_photometric'][i])==False:
-        metallicity_photometric = {'ref': str(dsph_mw_name['ref_metallicity_photometric'][i])}
-        for prop in ['metallicity_photometric', 'metallicity_photometric_em', 'metallicity_photometric_ep',   ]:
-            if prop in dsph_mw_name.dtype.names and ma.is_masked(dsph_mw_name[prop][i])==False:
-                metallicity_photometric[prop] = float(dsph_mw_name[prop][i])
-        d_t['metallicity_photometric'] = metallicity_photometric
-    
-    if ma.is_masked(dsph_mw_name['ref_flux_HI'][i])==False:
-        flux_HI = {'ref': str(dsph_mw_name['ref_flux_HI'][i])}
-        for prop in ['flux_HI', 'flux_HI_em', 'flux_HI_ep', 'flux_HI_ul'  ]:
-            if prop in dsph_mw_name.dtype.names and ma.is_masked(dsph_mw_name[prop][i])==False:
-                flux_HI[prop] = float(dsph_mw_name[prop][i]) 
-        d_t['flux_HI'] = flux_HI
-    notes_temp = notes[notes['key']==dsph_mw_name['key'][i]]
-    if len(notes_temp)>0:
-        notes_list = []
-        for kk in notes_temp['notes']:
-#             print(kk)
-            notes_list.append(str(kk))
-        d_t['notes'] = notes_list
-#     print(d_t)
-    yaml_name = 'data_input/'+ dsph_mw_name['key'][i]+'.yaml'
-    with open(yaml_name, 'w') as yaml_file:
-        yaml.dump(d_t, yaml_file, default_flow_style=False)
+    for i in range(len(loaded_table_name)):
+        d_t = {'key': str(loaded_table_name['key'][i]) }
+        
+        location = {'ra':float(loaded_table_name['ra'][i]), 'dec':float(loaded_table_name['dec'][i])}
+        d_t['location'] = location
+        
+        discovery = {}
+        for prop in ['name', 'table', 'host' ]:
+            if prop in loaded_table_name.dtype.names and ma.is_masked(loaded_table_name[prop][i])==False:
+                discovery[prop] = str(loaded_table_name[prop][i])
+        for prop in ['confirmed_real', 'confirmed_dwarf', 'confirmed_star_cluster', 'discovery_year']:
+            if prop in loaded_table_name.dtype.names and ma.is_masked(loaded_table_name[prop][i])==False:
+                discovery[prop] = int(loaded_table_name[prop][i])
+        ref_discovery_list = []
+        for prop in ['ref_discovery_1', 'ref_discovery_2', 'ref_discovery_3',]:
+            if ma.is_masked(loaded_table_name[prop][i])==False:
+                ref_discovery_list.append(str(loaded_table_name[prop][i]))
+        if len(ref_discovery_list)>0:
+            discovery['ref_discovery'] = ref_discovery_list
+        ref_other_name_list = []
+        for prop in ['other_name_1', 'other_name_2', 'other_name_3','other_name_4', 'other_name_5', 'other_name_6', ]:
+            if ma.is_masked(loaded_table_name[prop][i])==False:
+                ref_other_name_list.append(str(loaded_table_name[prop][i]))
+        if len(ref_other_name_list)>0:
+            discovery['other_name'] = ref_other_name_list
+        
+        d_t['name_discovery'] = discovery
+        
+        if ma.is_masked(loaded_table_name['ref_structure'][i])==False:
+            structure = {'ref_structure': str(loaded_table_name['ref_structure'][i]) }
+            for prop in ['rhalf', 'rhalf_em', 'rhalf_ep', 'position_angle', 'position_angle_em', 'position_angle_ep', 'ellipticity', 'ellipticity_em', 'ellipticity_ep', 'ellipticity_ul',]:
+                if prop in loaded_table_name.dtype.names and ma.is_masked(loaded_table_name[prop][i])==False:
+                    structure[prop] = float(loaded_table_name[prop][i])
+            d_t['structure'] = structure
+        
+        if ma.is_masked(loaded_table_name['ref_distance'][i])==False:
+            distance = {'ref_distance': str(loaded_table_name['ref_distance'][i]) }
+            for prop in ['distance', 'distance_em', 'distance_ep', 'distance_modulus', 'distance_modulus_em', 'distance_modulus_ep',]:
+                if prop in loaded_table_name.dtype.names and ma.is_masked(loaded_table_name[prop][i])==False:
+                    distance[prop] = float(loaded_table_name[prop][i])
+            d_t['distance'] = distance
+        
+        if ma.is_masked(loaded_table_name['ref_mv'][i])==False:
+            MV = {'ref_m_v':str(loaded_table_name['ref_mv'][i]), }
+            for prop_out,prop in zip( ['apparent_magnitude_v', 'apparent_magnitude_v_em', 'apparent_magnitude_v_ep'],['apparent_magnitude_V', 'apparent_magnitude_V_em', 'apparent_magnitude_V_ep']):
+                if prop in loaded_table_name.dtype.names and ma.is_masked(loaded_table_name[prop][i])==False:
+                    MV[prop_out] = float(loaded_table_name[prop][i])
+            d_t['m_v'] = MV
+        
+        if ma.is_masked(loaded_table_name['ref_vlos'][i])==False:
+            vlos = {'ref_vlos': str(loaded_table_name['ref_vlos'][i]) }
+            for prop in ['vlos_systemic', 'vlos_systemic_em', 'vlos_systemic_ep', 'vlos_sigma', 'vlos_sigma_em', 'vlos_sigma_ep', 'vlos_sigma_ul',]:
+                if prop in loaded_table_name.dtype.names and ma.is_masked(loaded_table_name[prop][i])==False:
+                    vlos[prop] = float(loaded_table_name[prop][i])
+            d_t['velocity'] = vlos
+        
+        if ma.is_masked(loaded_table_name['ref_proper_motion'][i])==False:
+            pm = {'ref_proper_motion': str(loaded_table_name['ref_proper_motion'][i]) }
+            for prop in ['pmra', 'pmra_em', 'pmra_ep', 'pmdec', 'pmdec_em', 'pmdec_ep', ]:
+                if prop in loaded_table_name.dtype.names and ma.is_masked(loaded_table_name[prop][i])==False:
+                    pm[prop] = float(loaded_table_name[prop][i])
+            d_t['proper_motion'] = pm
+        
+        if ma.is_masked(loaded_table_name['ref_metallicity'][i])==False:
+            spec_metallicity = {'ref_spec_metallicity': str(loaded_table_name['ref_metallicity'][i]) }
+            for prop in ['metallicity', 'metallicity_em', 'metallicity_ep', 'metallicity_sigma', 'metallicity_sigma_em', 'metallicity_sigma_ep', ]:
+                if prop in loaded_table_name.dtype.names and ma.is_masked(loaded_table_name[prop][i])==False:
+                    spec_metallicity['spec_'+prop] = float(loaded_table_name[prop][i])
+            d_t['spec_metallicity'] = spec_metallicity
+        
+        if ma.is_masked(loaded_table_name['ref_king'][i])==False:
+            king = {'ref_structure_king': str(loaded_table_name['ref_king'][i])}
+            for prop in ['rcore', 'rcore_em', 'rcore_ep', 'rking', 'rking_em', 'rking_ep', 'king_concentration', 'king_concentration_em', 'king_concentration_ep', ]:
+                if prop in loaded_table_name.dtype.names and ma.is_masked(loaded_table_name[prop][i])==False:
+                    king[prop] = float(loaded_table_name[prop][i])
+            d_t['structure_king'] = king
+        
+        if ma.is_masked(loaded_table_name['ref_sersic'][i])==False:
+            sersic = {'ref_structure_sersic': str(loaded_table_name['ref_sersic'][i])}
+            for prop in ['rad_sersic', 'rad_sersic_em', 'rad_sersic_ep', 'n_sersic', 'n_sersic_em', 'n_sersic_ep',  ]:
+                if prop in loaded_table_name.dtype.names and ma.is_masked(loaded_table_name[prop][i])==False:
+                    sersic[prop] = float(loaded_table_name[prop][i])
+            d_t['structure_sersic'] = sersic
+        
+        if ma.is_masked(loaded_table_name['ref_age'][i])==False:
+            age = {'ref_age': str(loaded_table_name['ref_age'][i])}
+            for prop in ['age', 'age_em', 'age_ep',   ]:
+                if prop in loaded_table_name.dtype.names and ma.is_masked(loaded_table_name[prop][i])==False:
+                    age[prop] = float(loaded_table_name[prop][i])
+            d_t['age'] = age
+        
+        if ma.is_masked(loaded_table_name['ref_metallicity_photometric'][i])==False:
+            metallicity_photometric = {'ref_metallicity_photometric': str(loaded_table_name['ref_metallicity_photometric'][i])}
+            for prop in ['metallicity_photometric', 'metallicity_photometric_em', 'metallicity_photometric_ep',   ]:
+                if prop in loaded_table_name.dtype.names and ma.is_masked(loaded_table_name[prop][i])==False:
+                    metallicity_photometric[prop] = float(loaded_table_name[prop][i])
+            d_t['metallicity_photometric'] = metallicity_photometric
+        
+        if 'ref_flux_HI' in loaded_table_name.dtype.name:
+            if ma.is_masked(loaded_table_name['ref_flux_HI'][i])==False:
+                flux_HI = {'ref_flux_HI': str(loaded_table_name['ref_flux_HI'][i])}
+                for prop in ['flux_HI', 'flux_HI_em', 'flux_HI_ep', 'flux_HI_ul'  ]:
+                    if prop in loaded_table_name.dtype.names and ma.is_masked(loaded_table_name[prop][i])==False:
+                        flux_HI[prop] = float(loaded_table_name[prop][i]) 
+                d_t['flux_HI'] = flux_HI
+                
+        notes_temp = notes[notes['key']==loaded_table_name['key'][i]]
+        if len(notes_temp)>0:
+            notes_list = []
+            for kk in notes_temp['notes']:
+    #             print(kk)
+                notes_list.append(str(kk))
+            d_t['notes'] = notes_list
+    #     print(d_t)
+        yaml_name = 'data_input/'+ loaded_table_name['key'][i]+'.yaml'
+        with open(yaml_name, 'w') as yaml_file:
+            yaml.dump(d_t, yaml_file, default_flow_style=False)
