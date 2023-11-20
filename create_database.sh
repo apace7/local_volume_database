88d349d468acc19ba2ee1036e00a91132e8d179b
Andrew Pace <pvpace1@gmail.com>
2023-11-19 21:30:18 -0500

    add ursa major 3/unions 1; update table/latex creation

--- create_database.sh
+++ create_database.sh
@@ -1,10 +1,12 @@
 #!/bin/zsh
 
-# source ~/.zshrc
+source ~/.zshrc
 
-# python code/combine_table_general.py
+python code/combine_table_general.py
 
-# python code/create_latex_table.py
+python code/create_latex_table.py
+
+read -s -k '?Press any key to continue.'
 
 cd table/
 
