736f5737a5707d5d6bd1acdb5b1b24f3fabada78
Andrew Pace <pvpace1@gmail.com>
2024-09-01 23:51:53 -0400

    add M101 systems

--- create_database.sh
+++ create_database.sh
@@ -4,9 +4,11 @@ source ~/.zshrc
 
 python code/combine_table_general.py
 
+print 'creating latex tables'
+
 python code/create_latex_table.py
 
-read -s -k '?Press any key to continue.'
+read -s -k '?Press any key to compile latex and create pdf.'
 
 cd table/
 
